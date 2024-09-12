4da23947569a466cc63769f5e96256387eaae603
Jinyun <imajinyun@gmail.com>
2024-09-12 11:30:38 +0800

    feat: add datetime util

---
+++ DateTime.go
@@ -0,0 +1,117 @@
+package gohelper
+
+import "time"
+
+type DateTime struct {
+	tim time.Time
+	loc *time.Location
+
+	layout string
+}
+
+func NewDateTime(date string, zone string, layout string) (*DateTime, error) {
+	loc, err := time.LoadLocation(zone)
+	if err != nil {
+		return nil, err
+	}
+
+	pst, err := time.Parse(layout, date)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DateTime{tim: pst, loc: loc, layout: layout}, nil
+}
+
+func NowDateTime(zone string, layout string) (*DateTime, error) {
+	loc, err := time.LoadLocation(zone)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DateTime{tim: time.Now(), loc: loc, layout: layout}, nil
+}
+
+func (d *DateTime) Now() string {
+	return d.tim.In(d.loc).Format(d.layout)
+}
+
+func (d *DateTime) Today() string {
+	return d.tim.In(d.loc).Format(d.layout)
+}
+
+func (d *DateTime) ToString() string {
+	return d.tim.In(d.loc).Format(d.layout)
+}
+
+func (d *DateTime) FmtDate() string {
+	return d.tim.In(d.loc).Format(time.DateOnly)
+}
+
+func (d *DateTime) FmtTime() string {
+	return d.tim.In(d.loc).Format(time.TimeOnly)
+}
+
+func (d *DateTime) FmtDateTime() string {
+	return d.tim.In(d.loc).Format(time.DateTime)
+}
+
+func (d *DateTime) Year() int {
+	return d.tim.In(d.loc).Year()
+}
+
+func (d *DateTime) Month() time.Month {
+	return d.tim.In(d.loc).Month()
+}
+
+func (d *DateTime) Day() int {
+	return d.tim.In(d.loc).Day()
+}
+
+func (d *DateTime) Hour() int {
+	return d.tim.In(d.loc).Hour()
+}
+
+func (d *DateTime) Minute() int {
+	return d.tim.In(d.loc).Minute()
+}
+
+func (d *DateTime) Second() int {
+	return d.tim.In(d.loc).Second()
+}
+
+func (d *DateTime) Nanosecond() int {
+	return d.tim.In(d.loc).Nanosecond()
+}
+
+func (d *DateTime) Weekday() time.Weekday {
+	return d.tim.In(d.loc).Weekday()
+}
+
+func (d *DateTime) Unix() int64 {
+	return d.tim.Unix()
+}
+
+func (d *DateTime) Yesterday() time.Time {
+	return d.tim.AddDate(0, 0, -1).In(d.loc)
+}
+
+func (d *DateTime) Tomorrow() time.Time {
+	return d.tim.AddDate(0, 0, 1).In(d.loc)
+}
+
+func (d *DateTime) LastWeek() time.Time {
+	return d.tim.AddDate(0, 0, -7).In(d.loc)
+}
+
+func (d *DateTime) NextWeek() time.Time {
+	return d.tim.AddDate(0, 0, 7).In(d.loc)
+}
+
+func (d *DateTime) BeginOfDay() time.Time {
+	return time.Date(d.tim.Year(), d.tim.Month(), d.tim.Day(), 0, 0, 0, 0, d.loc)
+}
+
+func (d *DateTime) EndOfDay() time.Time {
+	return time.Date(d.tim.Year(), d.tim.Month(), d.tim.Day(), 23, 59, 59, 0, d.loc)
+}
---
+++ DateTime_test.go
@@ -0,0 +1,377 @@
+package gohelper
+
+import (
+	"testing"
+	"time"
+)
+
+func TestNowDateTime_BeginEndOfDay(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now
+		outBegin := time.Date(out.Year(), out.Month(), out.Day(), 0, 0, 0, 0, loc).Format(tt.layout)
+		outEnd := time.Date(out.Year(), out.Month(), out.Day(), 23, 59, 59, 0, loc).Format(tt.layout)
+
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+		gotBegin, gotEnd := tim.BeginOfDay().Format(tt.layout), tim.EndOfDay().Format(tt.layout)
+
+		if gotBegin != outBegin {
+			t.Errorf("tim.ParseTime() = %s, want: %s", gotBegin, outBegin)
+		}
+
+		if gotEnd != outEnd {
+			t.Errorf("tim.ParseTime() = %s, want: %s", gotEnd, outEnd)
+		}
+	}
+}
+
+func TestNowDateTime_Day(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Day()
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.Day(); got != out {
+			t.Errorf("tim.Day() = %d, want: %d", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_FmtDate(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Format(time.DateOnly)
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.FmtDate(); got != out {
+			t.Errorf("tim.FmtDate() = %s, want: %s", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_FmtDateTime(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Format(time.DateTime)
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.FmtDateTime(); got != out {
+			t.Errorf("tim.FmtDateTime() = %s, want: %s", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_FmtTime(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Format(time.TimeOnly)
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.FmtTime(); got != out {
+			t.Errorf("tim.FmtTime() = %s, want: %s", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_Hour(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Hour()
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.Hour(); got != out {
+			t.Errorf("tim.Hour() = %d, want: %d", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_Minute(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Minute()
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.Minute(); got != out {
+			t.Errorf("tim.Minute() = %d, want: %d", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_Month(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Month()
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.Month(); got != out {
+			t.Errorf("tim.Month() = %d, want: %d", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_Now(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Format(tt.layout)
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.Now(); got != out {
+			t.Errorf("tim.Now() = %s, want: %s", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_Second(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Second()
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.Second(); got != out {
+			t.Errorf("tim.Second() = %d, want: %d", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_Today(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Format(tt.layout)
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.Today(); got != out {
+			t.Errorf("tim.Today() = %s, want: %s", got, out)
+		} else {
+			t.Log(got)
+		}
+	}
+}
+
+func TestNowDateTime_Tomorrow(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).AddDate(0, 0, 1).Format(tt.layout)
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		got := tim.Tomorrow().Format(tt.layout)
+		if got != out {
+			t.Errorf("tim.Tomorrow() = %s, want: %s", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_ToString(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Format(tt.layout)
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.ToString(); got != out {
+			t.Errorf("tim.ToString() = %s, want: %s", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_Unix(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Unix()
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.Unix(); got != out {
+			t.Errorf("tim.Unix() = %d, want: %d", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_Weekday(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Weekday()
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.Weekday(); got != out {
+			t.Errorf("tim.Weekday() = %d, want: %d", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_Year(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).Year()
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if got := tim.Year(); got != out {
+			t.Errorf("tim.Year() = %d, want: %d", got, out)
+		}
+	}
+}
+
+func TestNowDateTime_Yesterday(t *testing.T) {
+	now, tests := getNowDateTime()
+	for _, tt := range tests {
+		loc, err := time.LoadLocation(tt.zone)
+		if err != nil {
+			t.Error(err)
+		}
+
+		out := now.In(loc).AddDate(0, 0, -1).Format(tt.layout)
+		tim, err := NowDateTime(tt.zone, tt.layout)
+		if err != nil {
+			t.Error(err)
+		}
+
+		got := tim.Yesterday().Format(tt.layout)
+		if got != out {
+			t.Errorf("tim.Yesterday() = %s, want: %s", got, out)
+		}
+	}
+}
+
+func getNowDateTime() (time.Time, []struct {
+	in     time.Time
+	zone   string
+	layout string
+}) {
+	now := time.Now()
+	var tests = []struct {
+		in     time.Time
+		zone   string
+		layout string
+	}{
+		{now, "Asia/Shanghai", time.DateTime},
+		{now, "Asia/Singapore", time.RFC3339},
+		{now, "America/New_York", time.RFC1123},
+	}
+
+	return now, tests
+}
